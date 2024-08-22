ea091888730fc2f8ea9617e3c3531a6320393b37
samlee088 <108655429+samlee088@users.noreply.github.com>
2024-08-21 19:03:22 -0700

    hourly employee class

---
+++ group1/task5/code/HourlyEmployee.java
@@ -0,0 +1,19 @@
+package group1.task5.code;
+
+public class HourlyEmployee extends Employee{
+    private double wage;
+
+    HourlyEmployee(String name, int hours, double wage) {
+        super(name, hours);
+        this.wage = wage;
+    }
+    @Override
+    public String getType() {
+        return "Hourly";
+    }
+    @Override
+    public double totalPay() {
+        return this.hours * this.wage;
+    }
+    
+}
