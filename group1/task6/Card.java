2935c445421239e306bf995fc47dc317a63a526d
samlee088 <108655429+samlee088@users.noreply.github.com>
2024-08-21 20:37:23 -0700

    hand class

--- group1/task6/Card.java
+++ group1/task6/Card.java
@@ -25,3 +25,4 @@ public class Card {
         System.out.println(getSuit() + " " + getValue());
     }
 }
+
---
+++ group1/task6/Hand.java
@@ -0,0 +1,35 @@
+package group1.task6;
+import java.util.ArrayList;
+import java.util.List;
+import group1.task6.Card;
+
+public class Hand {
+    private List<Card> cards;
+    private int score;
+
+    public Hand() {
+        this.cards = new ArrayList<>();
+        this.score = 0;
+    }
+
+    public void addCard(Card card) {
+        cards.add(card);
+        if (card.getValue() == 1) {
+            score += (score + 11 <= 21) ? 11 : 1;
+        }
+    }
+
+    public int getScore() {
+        return score;
+
+    public List<Card> getCards() {
+        return cards;
+    }
+
+    public void print() {
+        for (Card card: cards) {
+            System.out.println(card.getSuit() + ", " + card.getValue());
+        }
+    }
+
+}
