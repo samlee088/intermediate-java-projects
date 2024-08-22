395199eb265da31d9ec351f4a9cf3562b5e07393
samlee088 <108655429+samlee088@users.noreply.github.com>
2024-08-21 19:10:50 -0700

    salaried employee

---
+++ group1/task5/code/SalariedEmployee.java
@@ -0,0 +1,20 @@
+package group1.task5.code;
+
+public class SalariedEmployee  extends Employee{
+    private double salary;
+    public SalariedEmployee (String name, int hours, double salary) {
+        super(name, hours);
+        this.salary = salary;
+    }
+
+    @Override
+    public String getType() {
+        return "Salaried";
+    }
+
+    @Override
+    public double totalPay() {
+        return salary / 52;
+    }
+
+}
