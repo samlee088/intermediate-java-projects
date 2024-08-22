4e01a2db3993e1d2d006c83f7a4ff9d2379d3d4f
samlee088 <108655429+samlee088@users.noreply.github.com>
2024-08-21 20:46:06 -0700

    player class

---
+++ group1/task6/Player.java
@@ -0,0 +1,23 @@
+package group1.task6;
+
+abstract class Player {
+    private Hand hand;
+
+    public Player(Hand hand) {
+        this.hand = hand;
+    }
+
+    public Hand gethand() {
+        return this.hand;
+    }
+
+    public void clearHand() {
+        this.hand = new Hand();
+    }
+
+    public void addCard(Card card) {
+        this.hand.addCard(card);
+    }
+
+    abstract boolean makeMove();
+}
