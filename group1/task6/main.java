31dd8529c07758e890f3fa042e5a15d71341c023
samlee088 <108655429+samlee088@users.noreply.github.com>
2024-08-22 17:03:48 -0700

    main class

--- group1/task6/main.java
+++ group1/task6/main.java
@@ -2,6 +2,12 @@ package group1.task6;
 
 
 
-public class main {
-    
+public class Main {
+    public static void main(String[] args) {
+        UserPlayer player = new UserPlayer(1000, new Hand());
+        Dealer dealer = new Dealer(new Hand());
+        while (player.getBalance() > 0) {
+            new GameRound(player, dealer, new Deck()).play();
+        }
+    }
 }
