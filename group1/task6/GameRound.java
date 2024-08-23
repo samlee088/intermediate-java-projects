8dccd2dc573fb29f4d55e831e8a6dc291881554b
samlee088 <108655429+samlee088@users.noreply.github.com>
2024-08-22 17:01:52 -0700

    game run round logic

--- group1/task6/GameRound.java
+++ group1/task6/GameRound.java
@@ -30,4 +30,56 @@ public class GameRound {
         System.out.println("Dealer's first card:");
         dealerCard.print();
     }
+
+    private void cleanupRound() {
+        player.clearHand();
+        dealer.clearHand();
+        System.out.println("Player balance: " + player.getBalance());
+    }
+
+    public void play() {
+        deck.shuffle();
+
+        if(player.getBalance() <= 0) {
+            System.out.println("Player has no more money =)");
+            return;
+        }
+
+        int userBet = getBetUser();
+        player.placeBet(userBet);
+
+        dealInitialCards();
+
+        while(player.makeMove()) {
+            Card drawnCard = deck.draw();
+            System.out.println("Player draws " + drawnCard.getSuit() + " " + drawnCard.getValue());
+            player.addCard(drawnCard);
+            System.out.println("Player score: " + player.getHand().getScore());
+        }
+        if(player.getHand().getScore() > 21) {
+            System.out.println("Player loses");
+            cleanupRound();
+            return;
+        }
+
+        dealer.updateTargetScore(player.getHand().getScore());
+        while(dealer.makeMove()) {
+            dealer.addCard(deck.draw());
+        }
+
+        int dealerScore = dealer.getHand().getScore();
+        int playerScore = player.getHand().getScore();
+        if (dealerScore > 21 || playerScore < dealerScore) {
+            System.out.println("Player wins");
+            player.receiveWinnings(userBet * 2);
+        } else if (dealerScore > playerScore) {
+            System.out.println("Player loses");
+        } else {
+            System.out.println("Game ends in a draw");
+            player.receiveWinnings(userBet);
+        }
+
+        cleanupRound();
+
+    }
 }
