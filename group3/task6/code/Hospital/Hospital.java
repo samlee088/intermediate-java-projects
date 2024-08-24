257323937bdca32d5bf953abc5d6aad046f14d14
samlee088 <108655429+samlee088@users.noreply.github.com>
2024-08-24 13:55:40 -0700

    initial commit for hospital class

--- group3/task6/code/Hospital/Hospital.java
+++ group3/task6/code/Hospital/Hospital.java
@@ -1,5 +1,27 @@
 package group3.task6.code.Hospital;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+
 public class Hospital {
-    
+    String name;
+    Map<String, Map<String, Object>> employees;
+    Map<String, Object> patients;
+    Queue <String> checkInQueue;
+    Queue<String> checkOutQueue;
+
+
+    public Hospital(String name) {
+        this.name = name;
+        employees = new HashMap<String, Map<String, Object>>();
+        Map<String, Object> receptionists = new HashMap<String, Object>();
+        Map<String, Object> doctors = new HashMap<String, Object>();
+        employees.put("receptionists", receptionists);
+        employees.put("doctors", doctors);
+        checkInQueue = new LinkedList<String>();
+        checkOutQueue = new LinkedList<String>();
+
+    }
 }
--- group3/task6/code/Main.java
+++ group3/task6/code/Main.java
@@ -1,8 +1,11 @@
 package group3.task6.code;
 
+import group3.task6.code.Hospital.Hospital;
+
 public class Main {
     
     public static void main(String[] args) {
-        
+        Hospital newportAnimalHospital = new Hospital("Newport Way Animal Hospital");
+
     }
 }
