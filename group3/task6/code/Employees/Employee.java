66446d3a712645a4797a1dd36b7da10fe2bd960a
samlee088 <108655429+samlee088@users.noreply.github.com>
2024-08-24 20:07:07 -0700

    employee working state

--- group3/task6/code/Employees/Employee.java
+++ group3/task6/code/Employees/Employee.java
@@ -3,14 +3,16 @@ package group3.task6.code.Employees;
 public class Employee {
     String name;
     Integer type;
+    Boolean currentlyWorking;
 
     public Employee(String name, Integer type) {
         this.name = name;
         this.type = type;
+        this.currentlyWorking = false;
     }
 
     public void working() {
-        return;
+        currentlyWorking = !currentlyWorking;
     }
 
     public String getEmployeeName() {
